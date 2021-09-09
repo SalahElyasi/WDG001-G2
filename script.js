9f110546dd01c0a2544a03e41025baa9c180fbde
Salah Elyasi <75736122+SalahElyasi@users.noreply.github.com>
2021-09-09 17:43:08 +0200

    bla bla

--- script.js
+++ script.js
@@ -6,3 +6,41 @@ function topFunction() {
 function alertFunction() {
   alert("Try to share Recipe...");
 }
+
+// -----------------random color-------------
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function setRandomColor() {
+  document.body.style.background = getRandomColor();
+}
+//----------------------
+function setDarkColor() {
+  document.body.style.background = "#666666";
+}
+function setLightColor() {
+  document.body.style.background = "#FFFFFF";
+}
+
+// ----------------------up---------------
+
+let btn = $("#button_up");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
